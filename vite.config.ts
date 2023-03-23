71db8f41d0e2068f3cbe9bc859d91972b2b8df9f
Dennis A. Boanini <dennis.boanini01@gmail.com>
2023-03-23 23:25:11 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-mentor-interactive-rating-component/",
   plugins: [react()],
 })
